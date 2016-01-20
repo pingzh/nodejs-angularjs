6d9befd13ce6053fa3065234703b3c3d440b4bab
Ping Zhang <pingzh@umich.edu>
2016-01-19 23:00:04 -0600

    barebone of nodejs-angularjs

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+// Create a app called scotchTodo
+// Create controllers under public/js/controllers/
+// Create directives under public/js/directives/
+var scotchTodo = angular.module('scotchTodo', []);
