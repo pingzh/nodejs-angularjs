93e24976d8f3e2c9cebec18153938cfcc7a41117
Ping Zhang <pingzh@umich.edu>
2016-01-26 20:28:29 -0600

    angular typeadead is done

--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -1,17 +1,32 @@
 function searchCtrl($scope, $http) {
 
-  $scope.departments = [
-    "eecs",
-    "mechang",
-    "eecs",
-    "ioe",
-    "naval",
-    "econ",
-    "phy",
-    "math",
-    "stats"
-  ];
+  $scope.selectedCourse = undefined;
+  $scope.getCourses = function(val) {
+    return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: val,
+        sensor: false
+      }
+    }).then(function(response){
+      return response.data.results.map(function(item){
+        return item.formatted_address;
+      });
+    });
+  };
   //$http calls api to get data
-}
-angular.module("psi").controller("searchCtrl", searchCtrl);
 
+  //fire a search
+  $scope.fireSearch = function() {
+    if($scope.selectedCourse != null) {
+      console.log($scope.selectedCourse);
+    }
+  }
+
+  $scope.didPressEnter = function(keyEvent) {
+    if(keyEvent.which == 13) {
+      $scope.fireSearch();
+    }
+  }
+}
+
+angular.module("psi").controller("searchCtrl", searchCtrl);
--- public/js/main.js
+++ public/js/main.js
@@ -8,20 +8,3 @@ $(document).ready(function() {
   }
 });
 
-// http://twitter.github.io/typeahead.js/examples/
-//
-// constructs the suggestion engine
-var countries = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  // url points to a json file that contains an array of country names, see
-  // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-  prefetch: 'js/data.json'
-});
-
-// passing in `null` for the `options` arguments will result in the default
-// options being used
-$('#prefetch .typeahead').typeahead(null, {
-  name: 'countries',
-  source: countries
-});
